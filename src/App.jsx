169fa1aa8ed3868987dd2395dc14214d36df81d2
Akshaypmna18 <akshaypmna18@gmail.com>
2023-11-15 20:00:40 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { Button } from "../components/ui/button";
+import QuizPage from "./quiz-page";
+
+function Home() {
+  return (
+    <section className="min-h-[100dvh] bg-[#F1DEDE] grid place-items-center">
+      <Link to="/quiz-page">
+        <Button className="uppercase text-[calc(1rem+1vw)] font-semibold tracking-wide border">
+          Start Quiz
+        </Button>
+      </Link>
+    </section>
+  );
+}
+
 function App() {
-  return <>hi</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz-page" element={<QuizPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/quiz-page.jsx
@@ -0,0 +1,5 @@
+export default function QuizPage() {
+  return (
+    <div className="min-h-[100dvh] absolute inset-0 bg-[#F1DEDE]">QuizPage</div>
+  );
+}
