90f006745c5e90ead7ba61c5e8c07f559073147d
Aкѕнαу <akshaypmnacoc@gmail.com>
2023-12-27 13:27:52 +0530

    Added 404 error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import QuizPage from "./Pages/QuizPage";
 import Home from "./Pages/Home";
+import ErrorPage from "./pages/NotFound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/quiz-page" element={<QuizPage />} />
+      <Route path="/*" element={<ErrorPage />} />
     </Routes>
   );
 }
